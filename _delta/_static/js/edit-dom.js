e0b9197683a7175a2db0704d469cd6d5bf60a24f
punknight <punknight16@gmail.com>
2019-10-09 13:41:43 -0700

    edit dom working

--- _delta/_static/js/edit-dom.js
+++ _delta/_static/js/edit-dom.js
@@ -1,5 +1,6 @@
 function editDOM(flag, data, AppContext){
 	//test case /row --add '[{"id":6,"tagname":"BUTTON","parent":0,"order":3},{"id":7,"tagname":"TEXT","parent":6,"order":0,"text":"HI"}]'
+	//test case /row --edit '[{"id":7,"tagname":"TEXT","parent":6,"order":0,"text":"SUBMIT"}]'
 	console.log('editDOM fired');
 	switch(flag){
 		case '--list':
@@ -13,6 +14,28 @@ function editDOM(flag, data, AppContext){
 				AppContext.dom_map.push(row);
 			});
 			break;
+		case '--edit':
+			var row_arr = JSON.parse(data.substring(1, data.length-1));
+			row_arr.map((row, index)=>{
+				editRow(row);
+			});
+			break;
+	}
+}
+
+function editRow(node){
+	//console.log('edit Row fired!');
+	if(node.tagname=='TEXT'){
+		var found_row = AppContext.dom_map.find((row)=>{
+			return (row.id == node.id)
+		});
+		if(found_row == undefined){
+			throw 'node_id does not exist';
+		}
+		found_row.model.nodeValue = node.text;
+		found_row.text = node.text;
+	} else {
+		console.log('might not ever have to edit a non-text node');
 	}
 }
 
